0debc5119aeef3c23623b25d1dfc2c4b14206cce
botbien <vubac2004@gmail.com>
2025-06-17 16:40:51 +0700

    [Module] add khach hang find function with MaKH

--- src/modules/NVKT/KhachHang/khachhangController.js
+++ src/modules/NVKT/KhachHang/khachhangController.js
@@ -1,5 +1,15 @@
 const KhachHang = require('./khachhangModel');
 const khachhangController = {
+    TimKH: async (req, res) => { 
+      try {
+        const {MaKH} = req.params;
+        const infoKH = await KhachHang.TimKH(parseInt(MaKH)); 
+        res.status(200).json(infoKH)
+      }
+      catch (err){
+        res.status(500).json({ error: err.message });
+      }
+    },
     LayThongTinKH: async (req, res) => {
         try {
             const {maDangKy} = req.query;
--- src/modules/NVKT/KhachHang/khachhangModel.js
+++ src/modules/NVKT/KhachHang/khachhangModel.js
@@ -1,6 +1,28 @@
-const {pool} = require('../../../config/db');
+const {pool, sql_Str, sql_Int} = require('../../../config/db');
 
 const KhachHang = {
+  TimKH: async(MaKH) => {
+    try{
+      const stmt = `
+      select * from KhachHang
+      where KhachHangID = @MaKH;
+      `;
+
+      await pool.connect();
+      const result = await pool.request()
+        .input('MaKH', sql_Int, MaKH)
+        .query(stmt);
+      // await pool.close();
+
+      if (result.recordset.length <= 0){
+        throw new Error('no customer found')
+      }
+      return result.recordset;
+    }
+    catch (err){
+			throw new Error('Error get information in khachhang: ' + err.message);
+    }
+  },
 	LayThongTinKH: async (maDangKy) => {
 		const query = `
 		select * from KhachHang kh
@@ -32,6 +54,7 @@ const KhachHang = {
 		AND (pdk.ThoiGianLap = '${ngayLap}' OR '' = '${ngayLap}')
 		AND (pdk.TinhTrangThanhToan is null or pdk.TinhTrangThanhToan = 0);
 		`;
+    console.log(query);
 		try {
             await pool.connect()
 			const result = await pool.request().query(query);
