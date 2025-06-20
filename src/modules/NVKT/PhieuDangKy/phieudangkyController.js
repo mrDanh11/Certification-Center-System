a3284e417c08117f0db502ea15401da68057a2a9
botbien <vubac2004@gmail.com>
2025-06-20 17:15:59 +0700

    [Module] Add tao phieu dang ky method for controller and model

--- src/modules/NVKT/PhieuDangKy/phieudangkyController.js
+++ src/modules/NVKT/PhieuDangKy/phieudangkyController.js
@@ -1,63 +1,109 @@
 const PhieuDangKy = require('./phieudangkyModel');
 const phieudangkyController = {
-    LayThongTinDK: async (req, res) => {
-        try {
-            const { maDangKy } = req.query;
-            const infoDK = await PhieuDangKy.LayThongTinDK(maDangKy);
+  LayThongTinDK: async (req, res) => {
+    try {
+      const { maDangKy } = req.query;
+      const infoDK = await PhieuDangKy.LayThongTinDK(maDangKy);
 
-            if (!infoDK || infoDK.length === 0) {
-                return res.status(404).json({ error: "Không tìm thấy phiếu đăng ký" });
-            }
+      if (!infoDK || infoDK.length === 0) {
+        return res.status(404).json({ error: "Không tìm thấy phiếu đăng ký" });
+      }
 
-            res.json(infoDK[0]);
-        } catch (err) {
-            res.status(500).json({ error: err.message });
-        }
-    },
+      return res.status(200).json(infoDK[0]);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
 
-    XacNhanThanhToan: async (req, res) => {
-        try {
-            const { maDangKy } = req.query;
-            const { TinhTrangThanhToan } = req.body;
-            const result = await PhieuDangKy.XacNhanThanhToan(maDangKy, TinhTrangThanhToan);
-            if (result) {
-                res.json({ message: 'Cập nhật thành công' });
-            } else {
-                res.status(400).json({ error: 'Cập nhật không thành công' });
-            }
-        } catch (err) {
-            res.status(500).json({ error: err.message });
-        }
-    },
+  XacNhanThanhToan: async (req, res) => {
+    try {
+      const { maDangKy } = req.query;
+      const { TinhTrangThanhToan } = req.body;
+      const result = await PhieuDangKy.XacNhanThanhToan(maDangKy, TinhTrangThanhToan);
+      if (result) {
+        res.json({ message: 'Cập nhật thành công' });
+      } else {
+        res.status(400).json({ error: 'Cập nhật không thành công' });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
 
-    HuyPhieuDangKy: async (req, res) => {
-        try {
-            const { maDangKy } = req.query;
-            const { TinhTrangHuy } = req.body;
-            const result = await PhieuDangKy.HuyPhieuDangKy(maDangKy, TinhTrangHuy);
-            if (result) {
-                res.json({ message: 'Hủy phiếu đăng ký thành công' });
-            } else {
-                res.status(400).json({ error: 'Hủy phiếu đăng ký không thành công' });
-            }
-        } catch (err) {
-            res.status(500).json({ error: err.message });
-        }
-    },
+  HuyPhieuDangKy: async (req, res) => {
+    try {
+      const { maDangKy } = req.query;
+      const { TinhTrangHuy } = req.body;
+      const result = await PhieuDangKy.HuyPhieuDangKy(maDangKy, TinhTrangHuy);
+      if (result) {
+        res.json({ message: 'Hủy phiếu đăng ký thành công' });
+      } else {
+        res.status(400).json({ error: 'Hủy phiếu đăng ký không thành công' });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
 
-    LayDSChuaThanhToan: async (req, res) => {
-        try {
-            const { maDangKy, ngayLap} = req.query;
-            const infoDK = await PhieuDangKy.LayDSChuaThanhToan(maDangKy,ngayLap);
+  LayDSChuaThanhToan: async (req, res) => {
+    try {
+      const { maDangKy, ngayLap } = req.query;
+      const infoDK = await PhieuDangKy.LayDSChuaThanhToan(maDangKy, ngayLap);
 
-            if (!infoDK || infoDK.length === 0) {
-                return res.status(404).json({ error: "Không tìm thấy phiếu đăng ký" });
-            }
+      if (!infoDK || infoDK.length === 0) {
+        return res.status(404).json({ error: "Không tìm thấy phiếu đăng ký" });
+      }
 
-            res.json(infoDK);
-        } catch (err) {
-            res.status(500).json({ error: err.message });
-        }
-    },
+      res.json(infoDK);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  XacNhanThanhToan: async (req, res) => {
+    try {
+      const { maDangKy } = req.query;
+      const { TinhTrangThanhToan } = req.body;
+      const result = await PhieuDangKy.XacNhanThanhToan(maDangKy, TinhTrangThanhToan);
+      if (result) {
+        res.json({ message: 'Cập nhật thành công' });
+      } else {
+        res.status(400).json({ error: 'Cập nhật không thành công' });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  HuyPhieuDangKy: async (req, res) => {
+    try {
+      const { maDangKy } = req.query;
+      const { TinhTrangHuy } = req.body;
+      const result = await PhieuDangKy.HuyPhieuDangKy(maDangKy, TinhTrangHuy);
+      if (result) {
+        res.json({ message: 'Hủy phiếu đăng ký thành công' });
+      } else {
+        res.status(400).json({ error: 'Hủy phiếu đăng ký không thành công' });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  TaoPhieuDangKy: async (req, res) => {
+    const { khachHangID } = req.body;
+
+    if (!khachHangID) {
+      return res.status(400).json({ error: 'KhachHangID is required' });
+    }
+    // TODO: add nhan vien tiep nhan lap and loai phieu
+    const result = await PhieuDangKy.TaoPhieuDangKy(khachHangID);
+
+    if (result.success) {
+      res.status(201).json({ phieuID: result.phieuID });
+    } else {
+      res.status(500).json({ error: result.error + 'In controller' });
+    }
+  },
 }
 module.exports = phieudangkyController;
--- src/modules/NVKT/PhieuDangKy/phieudangkyModel.js
+++ src/modules/NVKT/PhieuDangKy/phieudangkyModel.js
@@ -1,37 +1,37 @@
-const {pool} = require('../../../config/db');
+const { pool, sql_Int, sql_Str, sql_DateTime } = require('../../../config/db');
 
 const PhieuDangKy = {
-	LayThongTinDK: async (maDangKy) => {
-		const query = `
+  LayThongTinDK: async (maDangKy) => {
+    const query = `
 		select * from PhieuDangKy pdk
 		left join PhieuDonVi pdv on pdv.PhieuID=pdk.PhieuID
 		where pdk.PhieuID = '${maDangKy}' AND (pdk.TinhTrangHuy is null or pdk.TinhTrangHuy = 0)
 		ORDER BY pdk.ThoiGianLap DESC;
 		`;
-		try {
-            await pool.connect()
-			const result = await pool.request().query(query);
-            const test = result.recordset
-			return test 
-		} catch (err) {
-			throw new Error('Error get information in phieudangky: ' + err.message);
-		}
-	},
+    try {
+      await pool.connect()
+      const result = await pool.request().query(query);
+      const test = result.recordset
+      return test
+    } catch (err) {
+      throw new Error('Error get information in phieudangky: ' + err.message);
+    }
+  },
 
-	XacNhanThanhToan: async (maDangKy, TinhTrangThanhToan) => {
-		const query = `
+  XacNhanThanhToan: async (maDangKy, TinhTrangThanhToan) => {
+    const query = `
 		UPDATE PhieuDangKy
 		SET TinhTrangThanhToan = ${TinhTrangThanhToan}
 		WHERE PhieuID = ${maDangKy};
 		`;
-		try {
-            await pool.connect()
-			const result = await pool.request().query(query);
-			return result.rowsAffected > 0;
-		} catch (err) {
-			throw new Error('Error get information in phieudangky: ' + err.message);
-		}
-	},
+    try {
+      await pool.connect()
+      const result = await pool.request().query(query);
+      return result.rowsAffected > 0;
+    } catch (err) {
+      throw new Error('Error get information in phieudangky: ' + err.message);
+    }
+  },
 
 	HuyPhieuDangKy: async (maDangKy,TinhTrangHuy) => {
 		const query = `
@@ -71,7 +71,32 @@ const PhieuDangKy = {
 		} catch (err) {
 			throw new Error('Error get information in phieudangky: ' + err.message);
 		}
-	}
+	},
+
+  TaoPhieuDangKy: async (khachHangID, loaiPhieu = 'Cá Nhân', nvTiepNhanLap = null) => {
+    try {
+      await pool.connect();
+      const transaction = pool.transaction()
+      await transaction.begin();
+      const request = transaction.request();
+      const result = await request 
+        .input('KhachHangID', sql_Int, khachHangID)
+        .input('ThoiGianLap', sql_DateTime, new Date())
+        .input('LoaiPhieu', sql_Str, loaiPhieu)
+        .input('NVTiepNhanLap', sql_Int, nvTiepNhanLap)
+        .query(`
+        INSERT INTO PhieuDangKy (KhachHangID, ThoiGianLap, LoaiPhieu, NVTiepNhanLap)
+        OUTPUT INSERTED.PhieuID
+        VALUES (@KhachHangID, @ThoiGianLap, @LoaiPhieu, @NVTiepNhanLap)
+      `);
+
+      await transaction.commit();
+      return { success: true, phieuID: result.recordset[0].PhieuID };
+    } catch (err) {
+      console.error('Error creating PhieuDangKy:', err);
+      return { success: false, error: err.message + ' In Model ' };
+    }
+  },
 }
 
 module.exports = PhieuDangKy;
